3333e57ffcc1051db2f737711a28c86c21fffd04
Sven Slootweg <jamsoftgamedev@gmail.com>
2015-04-23 09:31:44 +0200

    Update utility scripts

--- setup-root.sh
+++ setup-root.sh
@@ -1,6 +1,5 @@
 set -e
 
-useradd pdfy
 cd /home/pdfy
 
 apt-get install -y authbind
@@ -9,6 +8,8 @@ npm install -g coffee-script forever
 touch /etc/authbind/byport/80
 chown pdfy /etc/authbind/byport/80
 chmod 755 /etc/authbind/byport/80
+
+chown -R pdfy:pdfy .
 
 su -c "./setup.sh" pdfy
 
