6d36e0ad44248c7a462242f6f1a4a1f16a7ab379
Sven Slootweg <jamsoftgamedev@gmail.com>
2015-04-23 10:22:03 +0200

    Yet another runscript fix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export NODE_ENV=production
 export PORT=80
-forever start -l ./logs/forever.reporter.log -o ./logs/stdout.reporter.log -e ./logs/stderr.reporter.log -c coffee ./error-reporter.coffee
-authbind --deep forever start -l ./logs/forever.app.log -o ./logs/stdout.app.log -e ./logs/stderr.reporter.log -c coffee ./bin/www.coffee
+forever start -a -l ./logs/forever.reporter.log -o ./logs/stdout.reporter.log -e ./logs/stderr.reporter.log -c coffee ./error-reporter.coffee
+authbind --deep forever start -a -l ./logs/forever.app.log -o ./logs/stdout.app.log -e ./logs/stderr.reporter.log -c coffee ./bin/www.coffee
